7968f84e83d7010c1a482e85d3bf1df1067df839
anshiiiee <limbanianshi18@gmail.com>
2026-08-31 13:00:07 -0400

    Completed Task 7

--- server.py
+++ server.py
@@ -9,9 +9,12 @@ def render_index_page():
 
 @app.route("/emotionDetector")
 def sent_analyzer():
-    text_to_analyze = request.args.get('textToAnalyze')
+    text_to_analyze = request.args.get("textToAnalyze")
 
     response = emotion_detector(text_to_analyze)
+
+    if response["dominant_emotion"] is None:
+        return "Invalid text! Please try again!"
 
     return (
         f"For the given statement, the system response is "
